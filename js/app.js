58e01f8c9cfabc2b88420ffd975aa43582dcab9e
AbhishekS-78 <thisisabhishek.78@gmail.com>
2026-06-10 16:47:09 +0530

    Handle form values and store as an object

--- js/app.js
+++ js/app.js
@@ -17,4 +17,29 @@ function navigateTo(e) {
   document.querySelector(`#${targetId}`).classList.add("active");
 }
 
+function getFormValues() {
+    return {
+      id: Date.now(),
+      objectName: document.querySelector('#object-name').value,
+      objectType: document.querySelector('#object-type').value,
+      date: document.querySelector('#obs-date').value,
+      notes: document.querySelector('#obs-notes').value,
+      location: document.querySelector('#obs-location').value,
+    }
+}
+
+function initForm() {
+  document.querySelector('#save-entry-btn').addEventListener('click', () => {
+    const entry = getFormValues();
+    console.log(entry);
+  //   Clear entries after logging
+    document.querySelector('#object-name').value = '';
+    document.querySelector('#object-type').value = '';
+    document.querySelector('#obs-date').value = '';
+    document.querySelector('#obs-notes').value = '';
+    document.querySelector('#obs-location').value = '';
+  });
+}
+
 initNavigation();
+initForm();
